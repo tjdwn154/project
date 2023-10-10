240c8c4147d3305d0306f8b51afb5c84ff8a746f
moon <msj07199@gmail.com>
2023-10-10 16:54:23 +0900

    server

--- src/components/Check.js
+++ src/components/Check.js
@@ -20,7 +20,14 @@ function Regist() {
       <h1>Member List</h1>
       <ul>
         {members.map((member) => (
-          <li key={member.memberId}>{member.memberName}</li>
+          <li key={member.memberId}>
+            <p>이름: {member.memberName}</p>
+            <p>아이디: {member.memberId}</p>
+            <p>Email: {member.email}</p>
+            <p>성별: {member.gender}</p>
+            <p>나이: {member.age}</p>
+            <p>전화번호: {member.phone}</p>
+          </li>
         ))}
       </ul>
     </div>
