fd257dc43ffa824e6450cc7346ba2ddb10021abb
piran-0 <minypodo@gmail.com>
2023-10-16 14:37:35 +0900

    위치조정시작+css추가적으로

--- src/component/TicketBuy/TicketCheck.js
+++ src/component/TicketBuy/TicketCheck.js
@@ -1,12 +1,24 @@
+import "./TicketCheck.css";
+
 const TicketCheck = () => {
   return (
-    <div>
+    <div id="ticketContent">
       <h2>티켓수령방법</h2>
-      <form>
-        <input type="radio" id="opt1" name="tickway" value="현장수령" checked />
-        <label for="opt1">현장수령</label>
-        <input type="radio" id="opt2" name="tickway" value="배송" />
-        <label for="opt2">배송</label>
+      <form id="ticketForm">
+        <div id="ticketInnerForm1">
+          <input
+            type="radio"
+            id="opt1"
+            name="tickway"
+            value="현장수령"
+            checked
+          />
+          <label for="opt1">현장수령</label>
+        </div>
+        <div id="ticketInnerForm2">
+          <input type="radio" id="opt2" name="tickway" value="배송" />
+          <label for="opt2">배송</label>
+        </div>
       </form>
     </div>
   );
