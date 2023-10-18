1805c8fb0bfc5036b2b8a04da92065dfae4bb408
piran-0 <minypodo@gmail.com>
2023-10-18 10:05:14 +0900

    예매자 확인정보

--- src/component/TicketBuy/CustomerCheck.js
+++ src/component/TicketBuy/CustomerCheck.js
@@ -1,6 +1,31 @@
 import "./CustomerCheck.css";
+import { useState } from "react";
 
 const CustomerCheck = () => {
+  const [name, setName] = useState("");
+  const [birth, setBirth] = useState("");
+  const [num, setNum] = useState("");
+  const [email, setEmail] = useState("");
+
+  const onNameHandler = (e) => {
+    setName(e.currentTarget.value);
+  };
+  const onBirthHandler = (e) => {
+    setBirth(e.currentTarget.value);
+  };
+  const onNumHandler = (e) => {
+    setNum(e.currentTarget.value);
+  };
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    console.log(name, birth, email, num);
+  };
+
   return (
     <div id="customerContent">
       <h2>예매자 확인</h2>
@@ -8,7 +33,14 @@ const CustomerCheck = () => {
         <ul>
           <li className="customer-formList">
             <label id="name">이름</label>
-            <input id="name" type="text" maxLength="4" required />
+            <input
+              id="name"
+              type="text"
+              maxLength="4"
+              required
+              value={name}
+              onChange={onNameHandler}
+            />
           </li>
           <li className="customer-formList">
             <label id="birth">생년월일</label>
@@ -18,6 +50,8 @@ const CustomerCheck = () => {
               required
               minLength="6"
               maxLength="6"
+              value={birth}
+              onChange={onBirthHandler}
             />
             <p className="birth-ex">
               예{")"} 980110 {"("}YYMMDD{")"}
@@ -29,38 +63,29 @@ const CustomerCheck = () => {
               className="num"
               id="num"
               type="text"
-              size="3"
-              minLength="3"
-              maxLength="3"
+              minLength="10"
+              maxLength="11"
               required
+              value={num}
+              onChange={onNumHandler}
             />
-            <p className="num">-</p>
-            <input
-              className="num"
-              id="num"
-              type="text"
-              size="3"
-              minLength="3"
-              maxLength="3"
-              required
-            />
-            <p className="num">-</p>
-            <input
-              className="num"
-              id="num"
-              type="text"
-              size="3"
-              minLength="3"
-              maxLength="3"
-              required
-            />
+            <p className="birth-ex">
+              예{")"} 01012345678 {'(" - "'}표시 없이 입력{")"}
+            </p>
           </li>
           <li className="customer-formList">
             <label id="email">이메일</label>
-            <input id="email" type="email" required />
+            <input
+              id="email"
+              type="email"
+              required
+              value={email}
+              onChange={onEmailHandler}
+            />
           </li>
         </ul>
       </form>
+      <button onClick={onSubmitHandler}></button>
     </div>
   );
 };
