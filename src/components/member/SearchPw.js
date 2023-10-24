3760e3752516f8dd48a86bb5f4a64793d8fac77b
Su <kkrladydtn@gmail.com>
2023-10-24 17:11:52 +0900

    Add_SearchID

---
+++ src/components/member/SearchPw.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+function SearchPw () {
+//   const navigate = useNavigate();
+
+//   const [inputValue, setInputValue] = useState({
+//     userName: '',
+//     userPhoneNumber: '',
+//   });
+  
+//   const { userName, userPhoneNumber } = inputValue;
+
+//   const handleInput = e => {
+//     const { name, value } = e.target;
+//     setInputValue({
+//       ...inputValue,
+//       [name]: value,
+//     });
+//   };
+
+//   const getIsActive = userName.length >= 1 && userPhoneNumber.length >= 1;
+
+//   const handleButtonValid = () => {
+//     if (!getIsActive) {
+//       alert('please fill in the blanks');
+//     } else {
+//       fetch(API.Find_ID, {
+//         method: 'POST',
+//         body: JSON.stringify({
+//           name: inputValue.userName,
+//           contact: inputValue.userPhoneNumber,
+//         }),
+//       })
+//         .then(res => res.json())
+//         .then(res => {
+//           if (res.result?.[0]) {
+//             navigate('/print-email', { state: res.result[0]});
+//             console.log(res.result[0]);
+//           } else {
+//             alert('잘못된 정보입니다.');
+//           }
+//         });
+//     }
+//   };
+  return (
+    <div
+//       style={{
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         height: "100vh",
+//       }}
+    >
+{/* //       <Container>
+
+//       </Container> */}
+    </div>
+  )
+}
+
+export default SearchPw; 
