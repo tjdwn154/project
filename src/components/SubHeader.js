b8e56890e661a5eae4539281e145bb13b5fd9d5a
jinach0i <ginach0irk@gmail.com>
2023-10-26 16:13:45 +0900

    고객센터 css 2차

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,28 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function SubHeader() {
   return (
     <header id="subHeader">
       <div className="inner">
         <nav id="nav">
           <h1>
-            <a title="티켓1번가로" href="/">
-              <img src="/sub_color_logo.png" alt="티켓1번가 로고" />
-            </a>
+            <Link title="티켓1번가로" to="/">
+              <img src="/color_logo.png" alt="티켓1번가 로고" />
+            </Link>
           </h1>
           <ul>
             <li>
-              <a href="/login" title="" className="on">
+              <Link to="/login" title="" className="on">
                 로그인
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/mypage" title="">
+              <Link to="/mypage" title="">
                 마이 페이지
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/cs" title="">
+              <Link to="/cs" title="">
                 고객센터
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
